cca148d02ed71596be20760d8fd14d4ed76d734c
chohyeokjun <chj3731@naver.com>
2025-10-22 22:48:07 +0900

    Initial commit

---
+++ src/main/java/tech/technomart/TechnoMartApplication.java
@@ -0,0 +1,13 @@
+package tech.technomart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnoMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/technomart/TechnoMartApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.technomart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
